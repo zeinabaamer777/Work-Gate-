1ce6be9979b195f203e1c7e7064c5f322d3fdb89
zeinabaamer777 <zeinabaamer157@gmail.com>
2020-08-25 12:02:51 +0200

    create activity and update done

--- src/app/model/activities.model.ts
+++ src/app/model/activities.model.ts
@@ -1,7 +1,7 @@
-export class Activities{    
-    activityId: number;
-    enName: string;
-    arName: string;
-    centralAdminArName: string;
-    centeralAdminEnName: string;
+export interface Activities{    
+    activityId: number,
+    enName: string,
+    arName: string,
+    centralAdminArName: string,
+    centeralAdminEnName: string
 } 
